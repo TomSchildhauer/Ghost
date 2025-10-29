f854004ead6c1061ae2e3175e98da4c4887fa3c5
Troy Ciesco <tmciesco@gmail.com>
2025-10-29 16:55:51 -0400

    add status column

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '46790a2bcbfb8806ffab8ad25c3fbef4';
+    const currentSchemaHash = '92df18e4e3d004efc1105d08343c28d9';
     const currentFixturesHash = '0877727032b8beddbaedc086a8acf1a2';
     const currentSettingsHash = 'bb8be7d83407f2b4fa2ad68c19610579';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
